852fff506e047c3ef526865f3f725f29da6a551a
Claudia <claudia.iacob@hotmail.com>
2023-02-06 21:00:53 +0100

    Cambios2

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,51 +1,15 @@
 import './bootstrap';
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 import { delay } from 'lodash';
+// import './bootstrap';
+// import Swal from 'sweetalert2';
+
+// window.Swal = Swal;
+// // CommonJS
+
+// import Swal from 'sweetalert2';
+
+// window.Swal = Swal;
 // CommonJS
 
-  
-// function eventListeners(){
-//   mi_form =document.querySelector('#form_Acept');
-//   mi_form.addEventListener('submit',validar);
-// }
 
-// function validar(e){
-//   e.preventDefault();
-
-//   boton_aceptar = document.querySelector('#boton_aceptar');
-
-//   if(boton_aceptar == 'success'){
-//     Swal.fire({
-// title: 'Ad accepted',
-// icon: 'success',
-// showConfirmButton: false,
-// timer: 1500
-// })
-//   }else
-//   Swal.fire({
-//     title: 'Ad declined',
-//     icon: 'success',
-//     showConfirmButton: false,
-//     timer: 1500
-//     })
-// }
-=======
-import Swal from 'sweetalert2';
-
-window.Swal = Swal;
-// CommonJS
-
-  
-
->>>>>>> Stashed changes
-=======
-import Swal from 'sweetalert2';
-
-window.Swal = Swal;
-// CommonJS
-
-  
-
->>>>>>> Stashed changes
   
